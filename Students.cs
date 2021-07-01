9d0cc151feaddcdd501385e94bb6ec2505ad5335
jimmianh <73330878+jimmianh@users.noreply.github.com>
2021-07-01 16:26:56 +0700

    nop bai 1

---
+++ Students.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bai1ClassObject
+{
+    class Students
+    {
+        string _studName = "James Aderson";
+        int age = 27;
+
+        void Display()
+        {
+            Console.WriteLine("Student name:"+ _studName);
+            Console.WriteLine("Student Age:"+age);
+        }
+
+        static void Main(string[] args)
+        {
+            Students students = new Students();
+            students.Display();
+        }
+    }
+}
